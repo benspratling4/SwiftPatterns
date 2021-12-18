df8ca73d7a09805ca0a322b6871df9b51ca15656
Ben Spratling IV <bspratling@mac.com>
2021-12-18 12:15:12 -0500

    Add JSONPrimitive

---
+++ Sources/SwiftPatterns/JSONPrimitive.swift
@@ -0,0 +1,85 @@
+//
+//  JSONPrimitive.swift
+//  
+//
+//  Created by Ben Spratling on 12/18/21.
+//
+
+import Foundation
+
+
+///When someone is like "hey, let's throw some random stuff in a json object without predetermined keys and types"
+///but you still want to make it Decodable
+public enum JSONPrimitive : Hashable {
+	case boolean(Bool)
+	case integer(Int)
+	case float(Double)
+	case string(String)
+	case array([JSONPrimitive])
+	case object([String:JSONPrimitive])
+	case null
+}
+
+
+fileprivate struct JSONPrimitiveCodingKey : CodingKey {
+	
+	init?(stringValue: String) {
+		self.stringValue = stringValue
+		intValue = nil
+	}
+	
+	var stringValue: String
+	var intValue: Int?
+	
+	init?(intValue: Int) {
+		self.intValue = intValue
+		stringValue = "\(intValue)"
+	}
+}
+
+
+public enum JSONPrimitiveDecodingError : Error {
+	case unknownFormat
+}
+
+
+extension JSONPrimitive : Decodable {
+	
+	public init(from decoder: Decoder) throws {
+		if var arrayContainer:UnkeyedDecodingContainer = try? decoder.unkeyedContainer() {
+			var primitives:[JSONPrimitive] = []
+			while let primitive = try? arrayContainer.decode(JSONPrimitive.self) {
+				primitives.append(primitive)
+			}
+			self = .array(primitives)
+		}
+		else if let objectContainer:KeyedDecodingContainer<JSONPrimitiveCodingKey> = try? decoder.container(keyedBy: JSONPrimitiveCodingKey.self) {
+			var values:[String:JSONPrimitive] = [:]
+			for key in objectContainer.allKeys {
+				if let value = try? objectContainer.decode(JSONPrimitive.self, forKey: key) {
+					values[key.stringValue] = value
+				}
+			}
+			self = .object(values)
+		}
+		else  {
+			let container:SingleValueDecodingContainer = try decoder.singleValueContainer()
+			if let boolean:Bool = try? container.decode(Bool.self) {
+				self = .boolean(boolean)
+			}
+			else if let integer:Int = try? container.decode(Int.self) {
+				self = .integer(integer)
+			}
+			else if let float:Double = try? container.decode(Double.self) {
+				self = .float(float)
+			}
+			else if let string:String = try? container.decode(String.self) {
+				self = .string(string)
+			}
+			else {
+				self = .null
+			}
+		}
+	}
+	
+}
---
+++ Tests/SwiftPatternsTests/JSONPrimitiveTests.swift
@@ -0,0 +1,129 @@
+//
+//  JSONPrimitiveTests.swift
+//  
+//
+//  Created by Ben Spratling on 12/18/21.
+//
+
+import Foundation
+
+import XCTest
+import SwiftPatterns
+
+class JSONPrimitiveTests: XCTestCase {
+	
+	func testBooleanDecode() throws {
+		let trueValue = try JSONDecoder().decode(JSONPrimitive.self, from: "true".data(using: .utf8)!)
+		XCTAssertEqual(trueValue, .boolean(true))
+		
+		let falseValue = try JSONDecoder().decode(JSONPrimitive.self, from: "false".data(using: .utf8)!)
+		XCTAssertEqual(falseValue, .boolean(false))
+		
+		if let _ = try? JSONDecoder().decode(JSONPrimitive.self, from: "yes".data(using: .utf8)!) {
+			XCTFail("shouldn't have been able to decode that")
+		}
+	}
+	
+	func testIntegerDecode() throws {
+		let testCases:[(String, JSONPrimitive)] = [
+			("-13", .integer(-13)),
+			("6783482039", .integer(6783482039)),
+		]
+		
+		for (stringValue, jsonValue) in testCases {
+			let data:Data = stringValue.data(using: .utf8)!
+			let decoded = try JSONDecoder().decode(JSONPrimitive.self, from: data)
+			XCTAssertEqual(decoded, jsonValue)
+		}
+	}
+	
+	
+	func testFloatDecode() throws {
+		let testCases:[(String, Double, Double)] = [
+			("-13.1", -13.1, 0.00001),
+			("6783482039.8", 6783482039.8, 0.00001),
+		]
+		
+		for (stringValue, jsonValue, precision) in testCases {
+			let data:Data = stringValue.data(using: .utf8)!
+			let decoded = try JSONDecoder().decode(JSONPrimitive.self, from: data)
+			guard case .float(let fValue) = decoded else {
+				XCTFail("shouldn't have been able to decode that")
+				continue
+			}
+			XCTAssertEqual(fValue, jsonValue, accuracy: precision)
+		}
+	}
+	
+	func testStringDecode() throws {
+		let testCases:[(String, JSONPrimitive)] = [
+			("\"\"", .string("")),
+			("\"a\"", .string("a")),
+			("\"abcdefghijklmnopqrstuvwxyz\"", .string("abcdefghijklmnopqrstuvwxyz")),
+		]
+		
+		for (stringValue, jsonValue) in testCases {
+			let data:Data = stringValue.data(using: .utf8)!
+			let decoded = try JSONDecoder().decode(JSONPrimitive.self, from: data)
+			XCTAssertEqual(decoded, jsonValue)
+		}
+	}
+	
+	func testArrayDecode() throws {
+		let testCases:[(String, JSONPrimitive)] = [
+			//empty case
+			("[]", .array([])),
+			//single item case
+			("[\"a\"]", .array([.string("a")])),
+			//multi item case
+			("[\"a\", 1234]", .array([.string("a"), .integer(1234)])),
+			//recursive case
+			("[[5]]", .array([.array([.integer(5)])])),
+			//internal boolean
+			("[true]", .array([.boolean(true)])),
+			//internal object
+			("[{}]", .array([.object([:])])),
+			//internal float - requires testing with accuracy
+//			("[1234.5678]", .array([.float(1234.5678)])),
+		]
+		
+		for (stringValue, jsonValue) in testCases {
+			let data:Data = stringValue.data(using: .utf8)!
+			let decoded = try JSONDecoder().decode(JSONPrimitive.self, from: data)
+			XCTAssertEqual(decoded, jsonValue)
+		}
+	}
+	
+	func testNullDecode() throws {
+		let testCases:[(String, JSONPrimitive)] = [
+			("null", .null),
+		]
+		
+		for (stringValue, jsonValue) in testCases {
+			let data:Data = stringValue.data(using: .utf8)!
+			let decoded = try JSONDecoder().decode(JSONPrimitive.self, from: data)
+			XCTAssertEqual(decoded, jsonValue)
+		}
+	}
+	
+	
+	func testObjectDecode() throws {
+		let testCases:[(String, JSONPrimitive)] = [
+			//empty case
+			("{}", .object([:])),
+			//single item case
+			("{\"a\":12345}", .object(["a":.integer(12345)])),
+			//multi item case
+			("{\"a\":12345, \"b\":\"abc\"}", .object(["a":.integer(12345), "b":.string("abc")])),
+			//recursive case
+			("{\"a\":{}}", .object(["a":.object([:])])),
+		]
+		
+		for (stringValue, jsonValue) in testCases {
+			let data:Data = stringValue.data(using: .utf8)!
+			let decoded = try JSONDecoder().decode(JSONPrimitive.self, from: data)
+			XCTAssertEqual(decoded, jsonValue)
+		}
+	}
+	
+}
